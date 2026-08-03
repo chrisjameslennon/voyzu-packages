de8e40d72cbc67451c8d6cc43ec7d9131953b1fc
Chris Lennon <chrisjameslennon@gmail.com>
2026-08-04 07:39:35 +1200

    Restore Core finance company switcher

---
+++ packages/@voyzu/core/navigation/left-nav-header.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import type { VoyzuSurfaceLeftNavHeaderProps } from "@voyzu/ui-surface/types";
+import { CompanySwitcher } from "../modules/company-switcher/client";
+
+export const leftNavHeaderRootPaths = ["/finance"] as const;
+
+export default function CoreLeftNavHeader({ isCollapsed }: VoyzuSurfaceLeftNavHeaderProps) {
+  return <CompanySwitcher isCollapsed={isCollapsed} />;
+}
