6eaabf457097ffcc93083e4cda1fda40caf708f0
Chris Lennon <chrisjameslennon@gmail.com>
2026-09-14 23:03:49 +1200

    Tidy Ugly Package route declarations

--- packages/@voyzu/ugly-package/modules/ugly/pages.routes.ts
+++ packages/@voyzu/ugly-package/modules/ugly/pages.routes.ts
@@ -1,6 +1,5 @@
 export const pageRoutes = {
   "voyzu.ugly-package.page.home": {
-
     httpApiDocumentationGroupId: "ugly-package.ugly",
     path: "/ugly-package",
     loadPage: () => import("./server/pages/UglyHomePage").then((module) => module.UglyHomePage),
@@ -8,7 +7,6 @@ export const pageRoutes = {
     auth: { required: true, minRole: "STANDARD" },
   },
   "voyzu.ugly-package.page.developer-freedom": {
-
     httpApiDocumentationGroupId: "ugly-package.ugly",
     path: "/ugly-package/developer-freedom",
     loadPage: () => import("./server/pages/DeveloperFreedomPage").then((module) => module.DeveloperFreedomPage),
@@ -16,7 +14,6 @@ export const pageRoutes = {
     auth: { required: true, minRole: "STANDARD" },
   },
   "voyzu.ugly-package.page.byo-dependencies": {
-
     httpApiDocumentationGroupId: "ugly-package.ugly",
     path: "/ugly-package/byo-dependencies",
     loadPage: () => import("./server/pages/ByoDependenciesPage").then((module) => module.ByoDependenciesPage),
