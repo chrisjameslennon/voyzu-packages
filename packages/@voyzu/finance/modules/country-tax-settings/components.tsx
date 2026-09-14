b61628767ca0dd9789c6ff4884310c8733079d07
Chris Lennon <chrisjameslennon@gmail.com>
2026-09-14 15:34:00 +1200

    Remove orphaned country tax settings component

--- packages/@voyzu/finance/modules/country-tax-settings/components.tsx
+++
@@ -1,9 +0,0 @@
-import { component } from "@voyzu/ui-surface/server";
-
-export const components = {
-  countryTaxSettings: component.defineLazy(
-    "settings.country-tax-settings",
-    () => import("./server/components/CountryTaxSettingsComponent")
-      .then((module) => module.CountryTaxSettingsComponent),
-  ),
-} as const;
